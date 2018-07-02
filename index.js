b6584184385c6bddf78bd9ab3a6509e4d5e2bfd5
ramesh aravinth <aravinthlr55@gmail.com>
2018-07-02 23:32:53 +0530

    adding JWT

--- index.js
+++ index.js
@@ -1,28 +1,49 @@
-const express= require('express');
+const express = require('express');
 const app = express();
 const secretPassword = 'MR.Secret';
 const fileSystem = require('fs');
 const bodyParser = require("body-parser"); // Body parser for fetch posted data
+const jwt = require('jsonwebtoken');
 
-app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json()); // Body parser use JSON data
 app.use(express.static('public'));
 
-app.get('/',function (req, res)  {
-    res.sendFile('/partials/login.html', {root: __dirname });
-}); 
 
-app.use('/',function(req, res, next) {
-    if(1) next();
-       else res.send('Trespassed');
+app.get('/login', function (req, res) {
+    res.sendFile('/partials/login.html', {
+        root: __dirname
+    });
 });
 
-app.post('/login',function(req, res) {
-    console.log(res.body.username);
-    res.send('saved successfully');
+app.post('/login', function (req, res) {
+    console.log(req.body);
+    var token = jwt.sign({
+        name: req.body.username
+    }, secretPassword, {
+        expiresIn: 86400 // expires in 24 hours
+    });
+    res.status(200).send({
+        auth: true,
+        token: token
+    });
 });
 
-app.get('/home',function(req, res) {
+
+app.use('/', function (req, res, next) {
+    console.log(req.get('token'));
+    if (!req.get('token')) {
+        res.sendFile('/partials/login.html', {
+            root: __dirname
+        });
+    } else next();
+});
+
+
+
+app.get('/home', function (req, res) {
     res.send('Hello World!');
 });
 
